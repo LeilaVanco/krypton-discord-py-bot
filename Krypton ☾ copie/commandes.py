12daab8ff2f3d594c968d0a312523f7eede2989f
Leïla Vanco <87393256+LeilaVanco@users.noreply.github.com>
2024-11-24 02:18:26 +0100

    Update commandes.py

--- Krypton ☾ copie/commandes.py
+++ Krypton ☾ copie/commandes.py
@@ -1,4 +1,5 @@
 import discord
+import time
 from discord.ext import commands
 
 token = "YOUR_TOKEN"
@@ -10,5 +11,32 @@ bot = commands.Bot(command_prefix = "!", intents=intents)
 async def test(ctx):
     await ctx.send("meow")
 
+@bot.command()
+async def decompte(ctx, delai: int):
+    await ctx.send("ATTENTION..")
+    for i in range(delai, 0, -1):
+        time.sleep(1)
+        await ctx.send(i)
+    await ctx.send("It's over !")
+
+@bot.command(
+        description = "This is a test",
+        brief = "another test",
+        help = "retest"
+)
+async def repete(ctx, *, message):
+    await ctx.send(message)
+
+@bot.event
+async def on_ready():
+    print(f"ALL IS OKAY, LOGGED ON {bot.user}")
+    try:
+        synced = await bot.sync()
+        print(f"{len(synced)} command(s) well synchronized !")
+    except Exception as error:
+        print(error)
+
+
+
 if __name__ == "__main__":
-    bot.run(token=token)
+    bot.run(token)
