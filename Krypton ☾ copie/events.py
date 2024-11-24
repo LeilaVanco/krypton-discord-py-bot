bd3e59b2175d1548543b1a0faa5e77c09b7a74b3
Leïla Vanco <87393256+LeilaVanco@users.noreply.github.com>
2024-11-24 02:21:29 +0100

    Update events.py

--- Krypton ☾ copie/events.py
+++ Krypton ☾ copie/events.py
@@ -7,10 +7,17 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_message(message: discord.Message):
     if message.author.bot:
         return
+        
+    if client.user.mentioned_in(message):
+        await message.channel.send("YUP IT'S ME THAT YOU JUST CALLED???")
+
+    if message.content == "no u":
+        await message.channel.send("no u")
 
 @client.event
 async def on_ready():
-    print("hi hi hi c pré ON COURT DANS LES PRéS!!!")
+    print("hi hi hi it's rEADY LET'S RUN to THE FIELDS !! (in french it sounds better...)")
     
 
-client.run(token=token)
+if __name__ == "__main__": 
+    client.run(token)
